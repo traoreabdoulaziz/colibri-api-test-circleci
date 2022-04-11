a0e1cf847be376b399b8ffb78ba16efe11986529
TRAORE Abdoul <abdoul.traore@data354.co>
2022-04-11 11:02:48 +0000

    add

--- app/api/endpoints/services.py
+++ app/api/endpoints/services.py
@@ -69,6 +69,8 @@ async def get_taxpayer_file(id: str,type:str,current_user: User_Pydantic = Depen
     return {"message": "success","code": "1","image_url":presigned_url}
 
 
+
+
 #Endpoint to download taxpayer image from S3
 @router.get("/download_activity_info/", tags=["S3 api"])
 async def get_activity_image(id: str,type:str,current_user: User_Pydantic = Depends(get_current_user)):
