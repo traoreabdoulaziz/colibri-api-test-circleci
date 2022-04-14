da470afc2d78b245459b6675fd3fce1a34424d26
TRAORE Abdoul <abdoul.traore@data354.co>
2022-04-14 15:56:48 +0000

    add

--- app/api/endpoints/services.py
+++ app/api/endpoints/services.py
@@ -16,7 +16,6 @@ router = APIRouter()
 DT_FMT_FN = '%Y%m%d%H%M%S'
 OUTPUT_BUCKET = 'data354-public-assets'
 
-
 #Endpoint to store image taxpayer on S3
 @router.post("/upload_taxpayer_info/", tags=["S3 api"])
 async def create_taxpayer_file(id: str,type:str, file: UploadFile = File(...), current_user: User_Pydantic = Depends(get_current_user)):
